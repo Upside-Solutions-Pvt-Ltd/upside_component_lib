e8e645dc4aceb9d1dbe22a4f8c3b0089bb513244
Levindu Herath <levinduherath@gmail.com>
2025-07-30 10:00:56 +0530

    tertiary button colors

--- lib/config/default_values.dart
+++ lib/config/default_values.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class ButtonConstants {
   static const double defaultRadius = 16.0;
   static const EdgeInsets buttonPadding = EdgeInsets.fromLTRB(24, 16, 24, 16);
+  static const double defaultBorderWidth = 1.0;
 }
--- lib/theme/app_colors.dart
+++ lib/theme/app_colors.dart
@@ -3,24 +3,28 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color primary = Color(0xFF38693C);
   static const Color onPrimary = Color(0xFFFFFFFF);
-
-  static const Color primaryHovered = Color(0xFF38693C);
-  static const Color primaryFocused = Color(0xFF38693C);
-  static const Color primaryPressed = Color(0xFF38693C);
-
+  static  Color primaryHovered = primary.withOpacity(0.08);
+  static  Color primaryFocused = primary.withOpacity(0.1);
+  static  Color primaryPressed = primary.withOpacity(0.08);
   static const Color primaryDisabled = Color(0x1A1D1B20);
   static const Color onPrimaryDisabled = Color(0xFF1D1B20);
 
   static const Color secondary = Color(0xFFCAC4D0);
   static const Color onSecondary = Color(0xFF49454F);
   static const Color secondaryBorder = Color(0xFFCAC4D0);
-
   static  Color secondaryHovered = secondary.withOpacity(0.08);
   static  Color secondaryFocused = secondary.withOpacity(0.1);
   static  Color secondaryPressed = secondary.withOpacity(0.08);
-
   static Color secondaryDisabled = secondary.withOpacity(0.1);
   static  Color onSecondaryDisabled = onSecondary;
+
+  static const Color tertiary = Color(0xFFE8DEF8);
+  static const Color onTertiary = Color(0xFF4A4459);
+  static Color tertiaryHovered = tertiary.withOpacity(0.08);
+  static Color tertiaryFocused = tertiary.withOpacity(0.1);
+  static Color tertiaryPressed = tertiary.withOpacity(0.08);
+  static  Color tertiaryDisabled = tertiary.withOpacity(0.1);
+  static const Color onTertiaryDisabled = Color(0xFF1D1B20);
 }
 
 
