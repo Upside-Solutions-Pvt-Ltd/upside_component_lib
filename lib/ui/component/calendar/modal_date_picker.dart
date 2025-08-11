78ae432bd118ecbc3bd0dbdd5e2d1e2bb2ab6e3a
Levindu Herath <levinduherath@gmail.com>
2025-08-11 08:47:27 +0530

    show time picker created

--- lib/ui/component/calendar/modal_date_picker.dart
+++ lib/ui/component/calendar/modal_date_picker.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:table_calendar/table_calendar.dart';
-import 'package:upside_component_lib/theme/app_colors.dart';
 
 class ModalDatePicker extends StatefulWidget {
   const ModalDatePicker({super.key});
@@ -10,45 +8,46 @@ class ModalDatePicker extends StatefulWidget {
 }
 
 class _ModalDatePickerState extends State<ModalDatePicker> {
-  DateTime? _selectedDay = DateTime.now();
-  DateTime _focusedDay = DateTime.now();
+  TimeOfDay? selectedTime;
+
+  Future<void> _pickTime() async {
+    final TimeOfDay? picked = await showTimePicker(
+      context: context,
+      initialTime: selectedTime ?? TimeOfDay.now(),
+      builder: (context, child) {
+        return Theme(data: ThemeData.light(), child: child!);
+      },
+    );
+
+    if (picked != null) {
+      setState(() {
+        selectedTime = picked;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return TableCalendar(
-      focusedDay: _focusedDay,
-      firstDay: DateTime.now().subtract(const Duration(days: 365)),
-      lastDay: DateTime.now().add(const Duration(days: 365)),
-      selectedDayPredicate: (day) {
-        return isSameDay(_selectedDay, day);
-      },
-
-      onDaySelected: (selectedDay, focusedDay) {
-        setState(() {
-          if (!isSameDay(_selectedDay, selectedDay)) {
-            _selectedDay = selectedDay;
-            _focusedDay = focusedDay;
-          }
-        });
-      },
-      calendarStyle: CalendarStyle(
-        selectedDecoration: BoxDecoration(
-          color: AppColors.calendarFocused,
-          shape: BoxShape.circle,
-        ),
-        todayDecoration: _selectedDay == null
-            ? BoxDecoration(color: Colors.transparent, shape: BoxShape.circle)
-            : BoxDecoration(
-                border: Border.all(color: AppColors.calendarFocusedBorder),
-                color: Colors.transparent,
-                shape: BoxShape.circle,
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: _pickTime,
+              child: const Text('Pick Time'),
+            ),
+            if (selectedTime != null)
+              Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: Text(
+                  'Selected Time: ${selectedTime!.format(context)}',
+                  style: const TextStyle(fontSize: 18),
+                ),
               ),
-        todayTextStyle: _selectedDay == null
-            ? TextStyle(color: Colors.white)
-            : TextStyle(color: Colors.black),
+          ],
+        ),
       ),
     );
   }
-
-  
 }
