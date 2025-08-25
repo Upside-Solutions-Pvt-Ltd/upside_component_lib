33438e16839845c0861b15586793ff72fcb2824f
Levindu Herath <levinduherath@gmail.com>
2025-08-25 12:49:55 +0530

    pointer value changes

--- lib/ui/component/gauges/radial_gauge.dart
+++ lib/ui/component/gauges/radial_gauge.dart
@@ -24,83 +24,81 @@ class RadialGauge extends StatefulWidget {
 class _RadialGaugeState extends State<RadialGauge> {
   @override
   Widget build(BuildContext context) {
-    return SfRadialGauge(
-      title: GaugeTitle(text: 'Temperature'),
-      axes: [
-        RadialAxis(
-          minimum: widget.firstPhaseStartValue,
-          maximum: widget.thirdPhaseEndValue,
-          labelOffset: -30,
-          showLastLabel: true,
-          interval: 10,
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return SfRadialGauge(
+          title: GaugeTitle(text: 'Temperature'),
+          axes: [
+            RadialAxis(
+              minimum: widget.firstPhaseStartValue,
+              maximum: widget.thirdPhaseEndValue,
+              labelOffset: -30,
+              showLastLabel: true,
+              interval: 10,
 
-          ranges: [
-            GaugeRange(
-              startValue: widget.firstPhaseStartValue,
-              endValue: widget.secondPhaseStartValue,
-              color: Colors.green,
-              startWidth: 35,
-              endWidth: 35,
-            ),
-            GaugeRange(
-              startValue: widget.secondPhaseStartValue,
-              endValue: widget.thirdPhaseStartValue,
-              color: Colors.orange,
-              startWidth: 35,
-              endWidth: 35,
-            ),
-            GaugeRange(
-              startValue: widget.thirdPhaseStartValue,
-              endValue: widget.thirdPhaseEndValue,
-              color: Colors.red,
-              startWidth: 35,
-              endWidth: 35,
-            ),
-          ],
-          pointers: [
-            NeedlePointer(
-              value: widget.value,
-              needleStartWidth: MediaQuery.of(context).size.width >= 750
-                  ? 1
-                  : 0.5,
-              needleEndWidth: MediaQuery.of(context).size.width >= 750 ? 10 : 5,
-              needleLength: MediaQuery.of(context).size.width >= 750
-                  ? 0.88
-                  : 0.7,
-              needleColor: Colors.black,
-              enableAnimation: true,
-              animationDuration: 1000,
-              animationType: AnimationType.ease,
-            ),
-          ],
-
-          annotations: [
-            GaugeAnnotation(
-              widget: Container(
-                decoration: BoxDecoration(
-                  color: Colors.grey,
-                  borderRadius: BorderRadius.circular(5),
+              ranges: [
+                GaugeRange(
+                  startValue: widget.firstPhaseStartValue,
+                  endValue: widget.secondPhaseStartValue,
+                  color: Colors.green,
+                  startWidth: 35,
+                  endWidth: 35,
                 ),
+                GaugeRange(
+                  startValue: widget.secondPhaseStartValue,
+                  endValue: widget.thirdPhaseStartValue,
+                  color: Colors.orange,
+                  startWidth: 35,
+                  endWidth: 35,
+                ),
+                GaugeRange(
+                  startValue: widget.thirdPhaseStartValue,
+                  endValue: widget.thirdPhaseEndValue,
+                  color: Colors.red,
+                  startWidth: 35,
+                  endWidth: 35,
+                ),
+              ],
+              pointers: [
+                NeedlePointer(
+                  value: widget.value,
+                  needleStartWidth: constraints.maxWidth >= 300 ? 0.9 : 0.5,
+                  needleEndWidth: constraints.maxWidth >= 300 ? 8 : 5,
+                  needleLength: constraints.maxWidth >= 300 ? 0.88 : 0.7,
+                  needleColor: Colors.black,
+                  enableAnimation: true,
+                  animationDuration: 1000,
+                  animationType: AnimationType.ease,
+                ),
+              ],
 
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    '${widget.value}°C',
-                    style: TextStyle(
-                      fontSize: MediaQuery.of(context).size.width >= 750
-                          ? 22
-                          : 14,
-                      fontWeight: FontWeight.bold,
+              annotations: [
+                GaugeAnnotation(
+                  widget: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey,
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        '${widget.value}°C',
+                        style: TextStyle(
+                          fontSize: constraints.maxWidth >= 300 ? 16 : 12,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                     ),
                   ),
+                  angle: 90,
+                  positionFactor: 0.5,
                 ),
-              ),
-              angle: 90,
-              positionFactor: 0.5,
+              ],
             ),
           ],
-        ),
-      ],
+        );
+      },
     );
   }
 }
