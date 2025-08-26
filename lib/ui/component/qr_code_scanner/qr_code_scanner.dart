f1f7858c063b6b1986a9f092e51d8ea24f67bc1b
Levindu Herath <levinduherath@gmail.com>
2025-08-26 15:00:10 +0530

    stop and start camera once scanned

--- lib/ui/component/qr_code_scanner/qr_code_scanner.dart
+++ lib/ui/component/qr_code_scanner/qr_code_scanner.dart
@@ -23,22 +23,32 @@ class _QRCodeScannerState extends State<QRCodeScanner> {
       height: 400,
       child: MobileScanner(
         controller: controller,
-        onDetect: (capture) {
+        onDetect: (capture) async {
           final List<Barcode> barcodes = capture.barcodes;
           final Uint8List? image = capture.image;
           for (final barcode in barcodes) {
             debugPrint('Barcode found! ${barcode.rawValue}');
           }
           if (image != null) {
+            await controller.stop();
             showDialog(
               context: context,
               builder: (context) {
                 return AlertDialog(
                   title: Text(barcodes.first.rawValue ?? ""),
                   content: Image(image: MemoryImage(image)),
+                  actions: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text('Close'),
+                    ),
+                  ],
                 );
               },
             );
+            await controller.start();
           }
         },
       ),
